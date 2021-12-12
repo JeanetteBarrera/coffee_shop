158df84d12a2cb66f59bbbabdb277869f39bca5d
JeanetteBarrera <jeanette.barrera0710@gmail.com>
2021-12-12 15:16:00 -0300

    modificaciones al proyecto original, para la clase extra

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -4,113 +4,37 @@ const path = require('path');
 const { productsExtra,categories, writeProductsExtraJson} = require("../data/data");// requiero las propiedades y metodos.
 
 let controller = {
-
+    /* Renderiza la vista del panel de control */
     index:(req, res) => {
-        res.render("admin/indexAdmin", {
-            products: productsExtra,
-            title: "Admin"
-        });
+        res.send("Panel del Administrador")
     },
+    /* Renderiza la vista de listar productos */
     listProducts: (req, res) => {
         res.render("admin/productList", {
             products: productsExtra,
             title: "List products"
         });
     }, 
+    /* Renderiza la vista de carga de productos */
     create: (req, res) => {
-        res.render("admin/productCreate",{
-            title: "Create Product"
-        });
+        res.send("Formulario de carga de producto")
     }, 
+    /* Metodo que se encarga de capturar toda la información que llega del formulario  y guardar un nuevo producto en la base de datos json*/
     addProduct: (req, res) => {
-        const { name, category, subcategory, description, price, discount, preparation} = req.body;
-        //definimos id, recorremos todo en array de objetos para capturar el ultimo id creado
-        let lastId = 1;
-        productsExtra.forEach(product => {
-            if(product.id > lastId) {
-                lastId = product.id
-            }
-        });
-
-        let newProduct = {
-            id: lastId + 1,
-            name: name.trim(),
-            category: category.trim(), 
-            subcategory: subcategory.trim(),
-            description: description.trim(),
-            price: price.trim(),
-            discount: discount.trim(),
-            status: true,
-            stock: 20,
-            img: req.file ? req.file.filename : "default-image.jpg",
-            preparation: preparation.trim()
-        }
-        productsExtra.push(newProduct);
-        writeProductsExtraJson(productsExtra);
-        res.redirect(`/admin/products`);
+        
     }, 
+    /* Renderiza la vista de edicion de productos, recibe un parametro id*/
     edit: (req, res) => {
+        res.send(`Formulario de edición de producto: ${req.params.id}`)
 
-        let productId = +req.params.id;
-        let product = productsExtra.find(product => product.id === productId );
-
-        res.render("admin/productEdit", {
-            product,
-            categories,
-            title : "Edit Product"
-        });
     },
+    /* Metodo que se encarga de capturar toda la información que llega del formulario de edición y actualizar la información en la base de datos json*/
     updateProduct:  (req, res) => {
         
-        const { name, category, subcategory, description, price, discount, preparation} = req.body;
-        let productId = +req.params.id;
-
-        productsExtra.forEach(product => {
-            if(product.id === productId) {
-                product.id = product.id,
-                product.name = name.trim(),
-                product.category = category.trim(),
-                product.subcategory = subcategory.trim(),
-                product.description = description.trim(),
-                product.price = price.trim(),
-                product.discount = discount.trim(),
-                product.stock = product.stock,
-                product.status = product.status
-                if(req.file){
-					if(fs.existsSync("./public/images/products/", product.img)){  
-						fs.unlinkSync(`./public/images/products/${product.img}`)
-					}
-					product.img = req.file.filename
-				}else{
-                    product.img = product.img
-				}
-                product.preparation = preparation.trim()
-            }
-        })
-        writeProductsExtraJson(productsExtra);
-        res.redirect(`/admin/products`);
     },
+    /*  */
     deleteProduct:  (req, res) => {
 
-        let productId = +req.params.id;
-
-		productsExtra.forEach(product => {
-
-			if(product.id === productId){
-
-                if(fs.existsSync("./public/images/products/", product.img)){ 
-                    fs.unlinkSync(`./public/images/products/${product.img}`)
-				}
-                let productDeleteIndex = productsExtra.indexOf(product)
-				
-				if(productDeleteIndex !== -1) {
-				    productsExtra.splice(productDeleteIndex, 1)
-				} 
-			}
-		})
-
-		writeProductsExtraJson(productsExtra)
-		res.redirect('/admin/products');
 	}
 }
 module.exports= controller;
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,21 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const upload = require('../middlewares/uploadProductFile');
 
-const {index, listProducts, create, addProduct, edit, updateProduct, deleteProduct} = require("../controllers/adminController");
+const{listProducts} = require("../controllers/adminController")
+/* Ruta que renderize el panel del administrador */
 
 
-//router.get("/", profile); //esta petición devuelve la vista del perfil de administrador
-router.get("/", index);
-router.get("/products", listProducts); // esta petición devuelve la vista de listar de todos los productos
+/* Ruta que renderize la lista de todos los productos */
+router.get("/products", listProducts); 
 
-router.get("/product", create); // esta petición devuelve la vista del formulario de carga de productos
-router.post("/product", upload.single('img'), addProduct); //esta petición crea un nuevo producto no devuelve nada
+/* Ruta que renderiza la vista de carga de productos */
 
-router.get("/product/:id", edit); // esta petición devuelve la vista del formulario de edición de productos, requiere un parametro obligatorio
-router.put("/product/:id",upload.single('img'), updateProduct); //actualiza producto no devuelve nada
 
-router.delete("/product/:id", deleteProduct); //elimina producto
+/* Ruta que se encargara de capturar la información que llega desde un formulario y guardarla en la base de datos json */
+
+
+/* Ruta que renderize la vista de edición de producto, debe recibir un parametro obligatorio para identificar el producto a editar */
+
+
+/* Ruta que se encargara de carturar la información que llega del formulario de edicion y actualizar la base de datos json */
+
+
+/* Ruta que se encargara de eliminar un producto de la base de datos, debe recibir un parametro obligatorio para identificar a el producto */
 
 
 module.exports = router;
